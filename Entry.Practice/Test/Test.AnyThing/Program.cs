a9e78a516facd9763918909d0ee4a1686b7b8af0
fighting71 <monster1720@163.com>
2021-03-08 21:19:20 +0800

    sync

--- Entry.Practice/Test/Test.AnyThing/Program.cs
+++ Entry.Practice/Test/Test.AnyThing/Program.cs
@@ -19,26 +19,41 @@ namespace Test.AnyThing
             //}
 
             //{
-            //    string txt = File.ReadAllText(@"C:/Users/Administrator/Desktop/newTest.html");
+            //    string txt = File.ReadAllText(@"C:/Users/Administrator/Desktop/newTest-format.html");
 
             //    string newTxt = Regex.Replace(txt, "style=\"color:#ff0000;[^\"]*\"", match =>
             //    {
-            //        return "style=\"color:#ff0000; \" class='answer'";
+            //        return "style=\"color:#ff0000; \"  class='answer'";
+            //    });
+
+            //    File.WriteAllText(@"C:/Users/Administrator/Desktop/newTest-format2.html", newTxt);
+            //}
+
+            //{
+            string txt = File.ReadAllText(@"C:/Users/Administrator/Desktop/data.txt");
+
+            string newTxt = Regex.Replace(txt, @"\d+\.", match =>
+            {
+                if(match.Value == "1945")
+                {
+
+                }
+                return string.Empty;
+            });
+
+            File.WriteAllText(@"C:/Users/Administrator/Desktop/data-format.txt", newTxt);
+            //}
+
+            //{
+            //    string txt = File.ReadAllText(@"C:/Users/Administrator/Desktop/newTest.html");
+
+            //    string newTxt = Regex.Replace(txt, @"\d+\.", match =>
+            //    {
+            //        return "<br><br><br>" + match.Value;
             //    });
 
             //    File.WriteAllText(@"C:/Users/Administrator/Desktop/newTest-format.html", newTxt);
             //}
-
-            {
-                string txt = File.ReadAllText(@"C:/Users/Administrator/Desktop/newTest.html");
-
-                string newTxt = Regex.Replace(txt, @"\d+\.", match =>
-                {
-                    return "<br><br><br>" + match.Value;
-                });
-
-                File.WriteAllText(@"C:/Users/Administrator/Desktop/newTest-format.html", newTxt);
-            }
 
             //{// 内容解析
 
